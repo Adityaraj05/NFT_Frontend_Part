2961333737b4ec7b968ffd413a2468dee0fdabed
Aditya Raj <adityarajavi05@gmail.com>
2024-08-25 13:12:58 +0530

    Working on Discover

--- components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx
+++ components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import { MdVerified } from "react-icons/md";
 import { TiTick } from "react-icons/ti";
 
+
 //INTERNAL IMPORT
 import Style from "./FollowerTabCard.module.css";
+
 const FollowerTabCard = ({ i, el }) => {
   const [following, setFollowing] = useState(false);
 
