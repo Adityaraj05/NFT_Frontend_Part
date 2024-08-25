d8b2366445975edd22a109f2b68477f3e1e1fcb7
Aditya Raj <adityarajavi05@gmail.com>
2024-08-25 18:08:17 +0530

    install react-dropzone and work on the account component

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
     <div className={Style.heroSection}>
       <div className={Style.heroSection_box}>
         <div className={Style.heroSection_box_left}>
-          <h1>Discover, collect, and sell NFTs 🖼️</h1>
+          <h1>NFTs-MARKET PALCE🖼️</h1>
           <p>
             Discover the most outstanding NTFs in all topics of life. Creative
             your NTFs and sell them.
